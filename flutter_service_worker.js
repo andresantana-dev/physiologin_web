4fffcef437728f0dc7a3f1277682ff0388eceff8
André Santana <andresantana.dev@gmail.com>
2025-05-17 17:20:26 +0100

    add code with flavor

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cc96c66d4e8cd9157693f334451a4a3a",
+const RESOURCES = {"flutter_bootstrap.js": "183fc0ef11140e3318925d7083daefb4",
 "version.json": "576761808a326182021602c899db3060",
 "index.html": "b10314546702f2d7feb74358a56ba0f0",
 "/": "b10314546702f2d7feb74358a56ba0f0",
-"main.dart.js": "a347505699324c3694e6da28d5d520a7",
+"main.dart.js": "dbb2bec9005e173a23ce90bb1b38077b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
